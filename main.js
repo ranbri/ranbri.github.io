f7a71f4d29e3cee41a451a1ba2f39bd33e26b983
ran bar ilan <rawown@gmail.com>
2021-11-23 19:36:18 +0200

    Facebook reviewed update

--- main.js
+++ main.js
@@ -1,23 +1,24 @@
 $(document).ready(function () {
-    $('.menu-toggler').on('click', function(){
+    $('.menu-toggler').on('click', function () {
         $(this).toggleClass('open');
         $('.top-nav').toggleClass('open');
     });
 
-    $('.top-nav .nav-link').on('click', function(){
+    $('.top-nav .nav-link').on('click', function () {
         $('.menu-toggler').toggleClass('open');
         $('.top-nav').toggleClass('open');
     });
 
-    $('nav a[href*="#]').on('click', function(){
+    $('nav a[href*="#]').on('click', function () {
         $('html,body').animate({
-            scrollTop: $($(this).attr('href')).offset().top-100
+            scrollTop: $($(this).attr('href')).offset().top - 100
         }, 2000);
     });
 
-    $('#up').on('click', function(){
+    $('#up').on('click', function () {
         $('html,body').animate({
-            scrollTop: 0 } ,2000);
+            scrollTop: 0
+        }, 2000);
     });
 
     AOS.init({
@@ -25,3 +26,56 @@ $(document).ready(function () {
         duration: 1800
     });
 });
+let skills = [
+    "Angular",
+    "NodeJS",
+    "Redux",
+    "REST API",
+    "MongoDB",
+    "jQuery",
+    "SQL",
+    "MySQL",
+    "JSON",
+    "Postman",
+    "ArduinoIDE",
+    "Git",
+    "Visual Studio Code",
+    "HTML",
+    "CSS",
+    "JavaScript",
+    "Excel",
+    "Fusion 360", 
+    "React",
+    "Bootstrap 4",
+    " Mobile First Design",
+    "Single Page Application",
+    "Type Script",
+    "Express.JS",
+    "Web Services API"
+]
+var table = document.getElementById('skills-table');
+function AddSkills() {
+    i = 0;
+    for (let skill of skills) {
+        var row = table.children[0].children[0];
+        var cell = row.insertCell(i)
+        cell.innerHTML = skill;
+        setColor(cell);
+    }
+}
+AddSkills();
+
+function getColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  }
+  
+  function setColor(child) {
+    child.style.setProperty('--shadowColor', getColor());
+  }
+  
+  
